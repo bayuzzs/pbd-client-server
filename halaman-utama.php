f3ce6694df2409636a690df05d7ab63a06643f4d
Bayu Maulana <bayufransdo@gmail.com>
2024-05-22 12:01:46 +0700

    debug

--- halaman-utama.php
+++ halaman-utama.php
@@ -64,7 +64,7 @@ $posts = $conn->query("SELECT * FROM post JOIN user ON (post.userId = user.userI
                                         class="bg-white  p-6 mb-6 shadow transition duration-100 group transform hover:shadow-lg rounded-2xl  border">
                                         <div class="relative mb-4 rounded-2xl overflow-hidden  ">
                                             <img class="max-h-32 rounded-2xl w-full object-cover bg-center transition-transform duration-100 transform group-hover:scale-105"
-                                                src="data:image/*;base64,<?= base64_encode($post['image']) ?>" alt="">
+                                                src="data:image/*;base64,<?= base64_encode($post['image']) ?>   " alt="">
                                         </div>
                                         <div class="flex justify-between items-center w-full pb-4 mb-auto">
                                             <div class="flex items-center">
