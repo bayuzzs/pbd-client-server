d48bf792b2478933793c5ffeae9014f31ebc69cb
Bayu Maulana <bayufransdo@gmail.com>
2024-05-22 11:16:14 +0700

    nambahin setting port

--- function/koneksi.php
+++ function/koneksi.php
@@ -1,10 +1,11 @@
 <?php
 $servername = "localhost";
+$port       = "3306";
 $username   = "root";
 $password   = "";
 $dbname     = "pesbuk";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ( $conn->connect_error ) {
     die("Koneksi gagal: " . $conn->connect_error);
